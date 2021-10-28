959272e4e975aef5ff0eebfa981d2dff345e2d40
AbhiPat123 <abhipat1996@gmail.com>
2021-10-29 01:00:19 +0300

    Data Loaders creation begins

--- Part1_Neural-Network-for-Collision-Prediction/collect_data.py
+++ Part1_Neural-Network-for-Collision-Prediction/collect_data.py
@@ -3,6 +3,7 @@
 from SteeringBehaviors import Wander
 import SimulationEnvironment as sim
 
+import pygame
 import numpy as np
 
 def collect_training_data(total_actions):
@@ -16,10 +17,13 @@ def collect_training_data(total_actions):
     num_params = 7
     #STUDENTS: network_params will be used to store your training data
     # a single sample will be comprised of: sensor_readings, action, collision
-    network_params =
+    network_params = np.zeros((100, 7))
 
 
     for action_i in range(total_actions):
+        for event in pygame.event.get():
+            pass
+
         progress = 100*float(action_i)/total_actions
         print(f'Collecting Training Data {progress}%   ', end="\r", flush=True)
 
@@ -42,12 +46,14 @@ def collect_training_data(total_actions):
 
 
         #STUDENTS: Update network_params.
+        network_params[action_i, :] = np.append(sensor_readings, [action, collision])
 
+    print("LOOP DONE! Saving CSV!")
 
     #STUDENTS: Save .csv here. Remember rows are individual samples, the first 5
     #columns are sensor values, the 6th is the action, and the 7th is collision.
     #Do not title the columns. Your .csv should look like the provided sample.
-
+    np.savetxt("submission.csv", network_params, delimiter=",")
 
 
 
